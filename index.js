b1f87d45061059a85685caf6252db52050dd56d0
Allie Robinson <allierob93@gmail.com>
2020-01-16 16:11:47 -0600

    change to index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require('./server');
 
 const port = process.env.PORT || 8000;
 
-server.listen(8000, () => {
-    console.log(`\n ** server running on http://localhost:8000 ** \n`);
+server.listen(port, () => {
+    console.log(`\n ** server running on http://localhost:${port} ** \n`);
 });
