bb9adca26f8f1b5fa2b1d1363820797629ec2b70
Allie Robinson <allierob93@gmail.com>
2020-01-16 15:24:39 -0600

    Heroku changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server');
 
+const port = process.env.PORT || 8000;
+
 server.listen(8000, () => {
     console.log(`\n ** server running on http://localhost:8000 ** \n`);
 });
